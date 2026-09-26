f7c3a141538155ec69b35ad1046114a16a84e709
bluecafe-fr <bluecafe.fr@gmail.com>
2026-09-26 12:08:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bmx-racing-lab-v1';
+const CACHE='bmx-racing-lab-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
